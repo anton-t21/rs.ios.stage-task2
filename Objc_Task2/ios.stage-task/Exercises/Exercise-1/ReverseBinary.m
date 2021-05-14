9c424c3545f4b0902551307cff09a938547f4e7d
AT <libra.giocondo@gmail.com>
2021-05-14 18:25:27 +0300

    feat: execute exercise 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,16 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+
+    NSString *str = @"" ;
+    NSUInteger x = n;
+
+    for (int i = 0; i < 8; i++) {
+        str = [str stringByAppendingString:[NSString stringWithFormat: @"%lu", x%2]];
+        x = x >> 1;
+    }
+
+    long result = strtol([str UTF8String], NULL, 2);
+
+    return result;
 }
