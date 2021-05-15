8723e91546f786366a6fa41dab7d54a500df948c
AT <libra.giocondo@gmail.com>
2021-05-15 19:13:18 +0300

    feat: execute exercise 2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,24 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    int count = (int)array.count;
+    int result = 0;
+    int globalStartPosition = 0;
+
+    for (NSNumber *key in array) {
+        int actualStartPosition = globalStartPosition;
+        for (int n = actualStartPosition; n < count - 1; n++) {
+            int currentElement = key.intValue;
+            int nextElement = array[actualStartPosition + 1].intValue;
+            int difference = abs(currentElement - nextElement);
+            if (difference == number.intValue) {
+                result = result + 1;
+            }
+            actualStartPosition++;
+        }
+        globalStartPosition = globalStartPosition + 1;
+    }
+    return result;
 }
 
 @end
